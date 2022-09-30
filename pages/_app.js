8aa47236d92da611370e76901a828fd8f8ff46ed
ismay <git.o8qty@simplelogin.com>
2022-09-30 18:26:55 +0200

    Add panelbear

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types, react/jsx-props-no-spreading, global-require */
 
 import "../styles/globals.css";
+import { usePanelbear } from "@panelbear/panelbear-nextjs";
 import Footer from "../components/footer";
 import Header from "../components/header";
 import LoadingIndicator from "../components/loading-indicator";
@@ -13,6 +14,7 @@ if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
 }
 
 export default function CustomApp({ Component, pageProps }) {
+  usePanelbear(process.env.NEXT_PUBLIC_PANELBEAR_SITE_ID);
   const isLoading = useLoading();
 
   return (
