bea5019d81bb56a42fdfeb20e10e7c7ce53d34aa
ismay <git.o8qty@simplelogin.com>
2022-09-30 13:59:43 +0200

    Wait for images to load before percy

--- .cypress/integration/home.test.js
+++ .cypress/integration/home.test.js
@@ -4,6 +4,8 @@ const links = [
   { href: "/work/three", name: "Three" },
 ];
 
+const images = ["One", "Two", "Three"];
+
 describe("home", () => {
   it("renders as expected", () => {
     cy.visit("/");
@@ -11,6 +13,18 @@ describe("home", () => {
     // Ensure expected links are all present
     links.forEach(({ href, name }) => {
       cy.findByRole("link", { name }).should("have.attr", "href", href);
+    });
+
+    // Ensure images have all been loaded before screenshotting
+    images.forEach((title) => {
+      cy.findByRole("img", { name: title })
+        .should("be.visible")
+        .and(($img) => {
+          expect(
+            $img[0].naturalWidth,
+            "image has natural width"
+          ).to.be.greaterThan(1);
+        });
     });
 
     cy.percySnapshot("home page renders as expected");
--- .cypress/integration/work-detail.test.js
+++ .cypress/integration/work-detail.test.js
@@ -2,6 +2,18 @@ describe("work detail", () => {
   it("renders as expected", () => {
     cy.visit("/work/one");
 
+    // Ensure images have all been loaded before screenshotting
+    cy.findAllByRole("img", { name: "One" }).each(($img) => {
+      cy.wrap($img)
+        .should("be.visible")
+        .and(() => {
+          expect(
+            $img[0].naturalWidth,
+            "image has natural width"
+          ).to.be.greaterThan(1);
+        });
+    });
+
     cy.percySnapshot("work detail page renders as expected");
   });
 });
