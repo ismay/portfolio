e7465dcb1b9559b7e420367369e699e8da981175
ismay <git.o8qty@simplelogin.com>
2022-10-03 19:56:05 +0200

    Add plaiceholder workaround

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+const { withPlaiceholder } = require("@plaiceholder/next");
+
+module.exports = withPlaiceholder({
   images: {
     domains: ["media.graphassets.com"],
   },
   reactStrictMode: true,
-};
+});
