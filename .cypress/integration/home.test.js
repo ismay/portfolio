2ec2b98a38ef951c65dada6b6546691cf93e0482
ismay <git.o8qty@simplelogin.com>
2022-10-07 19:16:38 +0200

    Wait for images to load before percy

--- .cypress/integration/home.test.js
+++ .cypress/integration/home.test.js
@@ -1,3 +1,7 @@
+const nextPrefix = "/_next/static/development";
+const devPagesManifest = `${nextPrefix}/_devPagesManifest.json`;
+const devMiddlewareManifest = `${nextPrefix}/_devMiddlewareManifest.json`;
+
 const links = [
   { href: "/work/one", name: "One" },
   { href: "/work/two", name: "Two" },
@@ -6,7 +10,11 @@ const links = [
 
 describe("home", () => {
   it("renders as expected", () => {
+    cy.intercept(devPagesManifest).as("devPagesManifest");
+    cy.intercept(devMiddlewareManifest).as("devMiddlewareManifest");
+
     cy.visit("/");
+    cy.wait(["@devPagesManifest", "@devMiddlewareManifest"]);
 
     // Ensure expected links are all present
     links.forEach(({ href, name }) => {
@@ -14,9 +22,17 @@ describe("home", () => {
     });
 
     // Ensure all images are done loading
-    cy.get("[data-next-image]")
-      .should("be.visible")
-      .and("have.attr", "data-loading", "false");
+    cy.get("[data-next-image]").should(($images) => {
+      expect($images.get(0)).to.be.visible;
+      expect($images.get(1)).to.be.visible;
+      expect($images.get(2)).to.be.visible;
+    });
+
+    cy.get("[data-next-image]").should(($images) => {
+      expect($images.get(0)).to.have.attr("data-loading", "false");
+      expect($images.get(1)).to.have.attr("data-loading", "false");
+      expect($images.get(2)).to.have.attr("data-loading", "false");
+    });
 
     cy.percySnapshot("home page renders as expected");
   });
--- .cypress/integration/work-detail.test.js
+++ .cypress/integration/work-detail.test.js
@@ -1,11 +1,27 @@
+const nextPrefix = "/_next/static/development";
+const devPagesManifest = `${nextPrefix}/_devPagesManifest.json`;
+const devMiddlewareManifest = `${nextPrefix}/_devMiddlewareManifest.json`;
+
 describe("work detail", () => {
   it("renders as expected", () => {
+    cy.intercept(devPagesManifest).as("devPagesManifest");
+    cy.intercept(devMiddlewareManifest).as("devMiddlewareManifest");
+
     cy.visit("/work/one");
+    cy.wait(["@devPagesManifest", "@devMiddlewareManifest"]);
 
     // Ensure all images are done loading
-    cy.get("[data-next-image]")
-      .should("be.visible")
-      .and("have.attr", "data-loading", "false");
+    cy.get("[data-next-image]").should(($images) => {
+      expect($images.get(0)).to.be.visible;
+      expect($images.get(1)).to.be.visible;
+      expect($images.get(2)).to.be.visible;
+    });
+
+    cy.get("[data-next-image]").should(($images) => {
+      expect($images.get(0)).to.have.attr("data-loading", "false");
+      expect($images.get(1)).to.have.attr("data-loading", "false");
+      expect($images.get(2)).to.have.attr("data-loading", "false");
+    });
 
     cy.percySnapshot("work detail page renders as expected");
   });
